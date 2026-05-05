e6f4ac3ea5ed1644c377895bf6f7bf5aa9aa4dde
Yulia <YULIIA.STUPNYTSKA@lnu.edu.ua>
2026-05-05 09:10:40 +0300

    lab3

--- lab3/Dyhotomia_class.cpp
+++ lab3/Dyhotomia_class.cpp
@@ -24,16 +24,19 @@ void Dyhotomia_class::setTolerance(double vol_eps)
 
 double Dyhotomia_class::f(double x)
 {
+    if (x == 0) return 0;
     return x - 2 + sin(1/x);
 }
 
 double Dyhotomia_class::df(double x)
 {
+    if (x == 0) return 0;
     return  1 - cos(1/x) / (x * x);
 }
 
 double Dyhotomia_class::solve_dyhotomia()
 {
+    if (f(a) * f(b) > 0) return 0;
     double c;
     while (b - a > eps)
     {
@@ -48,6 +51,7 @@ double Dyhotomia_class::solve_dyhotomia()
 
 double Dyhotomia_class::solve_newton()
 {
+    if (f(a) * f(b) > 0) return 0;
     double x = (a + b) / 2;
     double x_old;
     do
--- lab3/main.cpp
+++ lab3/main.cpp
@@ -8,11 +8,12 @@ int main()
 
     Dyhotomia_class *dyh = new Dyhotomia_class();
 
-    dyh->setVolumes(0.9, 2);
     dyh->setTolerance(0.00001);
 
+    dyh->setVolumes(0.9, 2);
     cout << "Dyhotomia method: " << dyh->solve_dyhotomia() << endl;
 
+    dyh->setVolumes(0.9, 2);
     cout << "Newton method: " << dyh->solve_newton() << endl;
 
     delete dyh;
